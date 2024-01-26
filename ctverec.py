426c0ee80c31ca7a91853a4d12e5c1cc3fb98074
Vaclav <vasek.stajger@centrum.cz>
2024-01-26 11:34:47 +0100

    vypln ctverce

--- ctverec.py
+++ ctverec.py
@@ -3,11 +3,11 @@ from turtle import Turtle, Screen
 zelva=Turtle()
 zelva.shape("turtle")
 zelva.color("black","green")
-
+zelva.begin_fill()
 for i in range(0,4):
     zelva.speed(2)
     zelva.forward(100)
     zelva.right(90)
-
+zelva.end_fill()
 obrazovka=Screen()
 obrazovka.exitonclick()
